7a3b634bd32e00903657ba509680845c8789b328
njoki <njerinjoki22@gmail.com>
2025-12-07 06:32:29 +0300

    add link to projectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,24 +1,21 @@
-import {FaGithub} from "react-icons/fa6";
 import {ArrowUpRight} from "lucide-react";
 
 const ProjectCard = ({ project }) => {
-    const {image, title, tags, liveLink} = project;
+    const {image, title, liveLink} = project;
     return (
         <div className="w-full h-full flex flex-col gap-2 p-2 overflow-hidden">
             <div className="h-5/6 lg:h-4/5">
+            <a href={liveLink} target="blank">
                 <img 
                     src={image} 
                     alt={title}
                     className="w-full h-full object-cover cursor-pointer" 
-                    />
+                />
+            </a>
+                
             </div>
             <section className="flex justify-between">
                 <h2 className="font-montserrat text-base text-stone-300">{title}</h2>
-                <div className="hidden">
-                    {tags?.map(tag => (
-                        <span key={tag}> #{tag} </span>
-                    ))}
-                </div>
                 <div>
                     <a href={liveLink} target="_blank">
                         <ArrowUpRight className="hover:text-(--coral-blue) fa-solid"/>
